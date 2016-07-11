e2aa46fc960701e34169c298d6c4bf58ff9dddd5
Riccardo M. Cefala <riccardo.cefala@mendix.com>
2016-07-11 13:05:25 +0200

    add dumb script to start programs I use every day

---
+++ scripts/startup_mendix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+i3-msg "workspace 3; exec gnome-terminal"
+
+sleep 1
+
+i3-msg "workspace 2; \
+        exec google-chrome --new-window \
+        https://sprintr.home.mendix.com \
+        https://trello.com ; \
+        exec slack" &
+
+sleep 1
+
+i3-msg "workspace 1; \
+        exec google-chrome --new-window \
+            https://webmail.mendix.com/ \
+            https://inbox.google.com/ \
+            https://web.whatsapp.com/" &
+
