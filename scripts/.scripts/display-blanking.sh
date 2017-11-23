ac5872ada7e57436d33c9af38da5d4b38024eb51
Riccardo M. Cefala <riccardo.cefala@gmail.com>
2017-11-23 09:51:26 +0100

    scripts: update display scripts

--- scripts/.scripts/display-blanking.sh
+++ scripts/.scripts/display-blanking.sh
@@ -4,10 +4,12 @@ ENABLED=$(xset q | sed -n 's/.*DPMS is \(Enabled\).*/\1/p')
 
 if [ $ENABLED ]
 then
+ echo disabled
  xset -dpms 
  xset s off
  xautolock -disable
 else
+ echo enabled
  xset +dpms 
  xset s on
  xautolock -enable
