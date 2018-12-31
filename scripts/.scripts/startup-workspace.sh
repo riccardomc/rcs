08dcde4e025896984d7a143eeb5824f9c1b54dba
Riccardo Cefala <riccardo.cefala@container-solutions.com>
2018-12-31 10:04:26 +0000

    startup-workspace.sh: work around chrome google accoutn login magic

--- scripts/.scripts/startup-workspace.sh
+++ scripts/.scripts/startup-workspace.sh
@@ -7,6 +7,6 @@ sleep 0.2
 i3-msg "workspace 1; \
         exec slack;\
         exec google-chrome --new-window \
-            https://calendar.google.com/ \
-            https://inbox.google.com/ \
+            https://calendar.google.com/calendar/b/1/r?u=1 \
+            https://mail.google.com/mail/u/1/ \
             https://web.whatsapp.com/;" &
