3361723cc6681c4214adae2ad2e06fbf8f91042a
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2020-04-29 11:18:36 +0200

    display names introduced a '-' so scripts need updating

--- scripts/.scripts/display-config.py
+++ scripts/.scripts/display-config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from Xlib import X, display
 
 
@@ -147,4 +147,4 @@ class MyRandr(object):
 
 if __name__ == "__main__":
     r = MyRandr()
-    r.set_from_left_to_right("eDP1")
+    r.set_from_left_to_right("eDP-1")
