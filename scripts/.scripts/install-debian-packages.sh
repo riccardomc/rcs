f26a8ee02ac207e8c44e345d651acfc95dab7ae4
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2020-05-02 17:29:36 +0200

    script: install debian packages

---
+++ scripts/.scripts/install-debian-packages.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+apt install -y \
+    apt-transport-https \
+    blueman \
+    build-essential \
+    ca-certificates \
+    caffeine \
+    compton \
+    curl \
+    diodon \
+    docker-ce \
+    feh \
+    flake8 \
+    git \
+    gnupg \
+    gparted \
+    i3 \
+    i7z \
+    jq \
+    kdiff3 \
+    laptop-mode-tools \
+    libcairo2-dev \
+    libdbus-glib-1-dev \
+    libgirepository1.0-dev \
+    libnotify-bin \
+    linux-cpupower \
+    maim \
+    nmap \
+    nmon \
+    pasystray \
+    pavucontrol \
+    pipewire \
+    powertop \
+    python3-dev \
+    python3-venv \
+    python3-wheel \
+    python3-xlib \
+    redshift \
+    rofi \
+    shellcheck \
+    software-properties-common \
+    solaar-gnome3 \
+    tig \
+    unrar \
+    virtualenvwrapper \
+    xautolock \
+    xbacklight \
+    xbacklight \
+    xdg-desktop-portal-gtk \
+    xournal \
+    xsel \
+    xserver-xorg-input-synaptics \
+    zim \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting
