fe0f88c0824b8c4873731bcf8ac0d0e4b83fa87e
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2020-09-01 15:04:29 +0200

    chezmoi: add completion for ZSH, but it doesn't work

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,7 @@ PATH=~/.bin:$PATH
 #chezmoi init --apply --verbose --dry-run https://github.com/riccardomc/rcs.git
 chezmoi init https://github.com/riccardomc/rcs.git
 chezmoi diff
+
+COMPLETION_DST="$HOME/.oh-my-zsh/completions"
+mkdir -p "$COMPLETION_DST"
+chezmoi completion zsh > "$COMPLETION_DST/_chezmoi" 
